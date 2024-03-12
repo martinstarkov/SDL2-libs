74226c601243f7fe4e27f78a734c85975da1de50
Martin <emailregisterlogin@gmail.com>
2024-03-12 01:18:30 +0100

    Add rest of the SDL libraries with static modified CMakes

--- SDL2-2.30.0/cmake/sdl2-config.cmake
+++ SDL2-2.30.0/cmake/sdl2-config.cmake
@@ -48,7 +48,7 @@ set(SDL2_INCLUDE_DIRS           "${SDL2_INCLUDE_DIR}")
 set_and_check(SDL2_BINDIR       "${SDL2_PREFIX}/lib/${_sdl_arch_subdir}")
 set_and_check(SDL2_LIBDIR       "${SDL2_PREFIX}/lib/${_sdl_arch_subdir}")
 
-set(SDL2_LIBRARIES      SDL2::SDL2main SDL2::SDL2-static)
+set(SDL2_LIBRARIES      SDL2::SDL2main SDL2::SDL2)
 set(SDL2MAIN_LIBRARY    SDL2::SDL2main)
 set(SDL2TEST_LIBRARY    SDL2::SDL2test)
 
@@ -69,9 +69,9 @@ set(_sdl2_link_libraries ${_sdl2_libraries})
 
 set(_sdl2_library "${SDL2_LIBDIR}/SDL2.lib")
 if(EXISTS "${_sdl2_library}")
-  if(NOT TARGET SDL2::SDL2-static)
-    add_library(SDL2::SDL2-static STATIC IMPORTED)
-    set_target_properties(SDL2::SDL2-static
+  if(NOT TARGET SDL2::SDL2)
+    add_library(SDL2::SDL2 STATIC IMPORTED)
+    set_target_properties(SDL2::SDL2
       PROPERTIES
       IMPORTED_LOCATION "${_sdl2_library}"
       INTERFACE_INCLUDE_DIRECTORIES "${SDL2_INCLUDE_DIRS}"
---
+++ SDL2_ttf-2.22.0/cmake/sdl2_ttf-config.cmake
@@ -0,0 +1,58 @@
+# SDL2_ttf CMake configuration file:
+# This file is meant to be placed in a cmake subfolder of SDL2_ttf-devel-2.x.y-VC
+
+include(FeatureSummary)
+set_package_properties(SDL2_ttf PROPERTIES
+    URL "https://www.libsdl.org/projects/SDL_ttf/"
+    DESCRIPTION "Support for TrueType (.ttf) font files with Simple Directmedia Layer"
+)
+
+cmake_minimum_required(VERSION 3.0)
+
+set(SDL2_ttf_FOUND TRUE)
+
+set(SDL2TTF_VENDORED TRUE)
+
+set(SDL2TTF_HARFBUZZ TRUE)
+set(SDL2TTF_FREETYPE TRUE)
+
+if(CMAKE_SIZEOF_VOID_P STREQUAL "4")
+    set(_sdl_arch_subdir "x86")
+elseif(CMAKE_SIZEOF_VOID_P STREQUAL "8")
+    set(_sdl_arch_subdir "x64")
+else()
+    unset(_sdl_arch_subdir)
+    set(SDL2_ttf_FOUND FALSE)
+    return()
+endif()
+
+set(_sdl2ttf_incdir       "${CMAKE_CURRENT_LIST_DIR}/../include")
+set(_sdl2ttf_library      "${CMAKE_CURRENT_LIST_DIR}/../lib/${_sdl_arch_subdir}/SDL2_ttf.lib")
+set(_sdl2ttf_dll          "${CMAKE_CURRENT_LIST_DIR}/../lib/${_sdl_arch_subdir}/SDL2_ttf.dll")
+
+set(_sdl2ttf_extra_static_libraries " -lusp10 -lgdi32 -lrpcrt4  -lusp10 -lgdi32 -lrpcrt4")
+string(STRIP "${_sdl2ttf_extra_static_libraries}" _sdl2ttf_extra_static_libraries)
+
+# Convert _sdl2ttf_extra_static_libraries to list and keep only libraries
+string(REGEX MATCHALL "(-[lm]([-a-zA-Z0-9._]+))|(-Wl,[^ ]*framework[^ ]*)" _sdl2ttf_extra_static_libraries "${_sdl2ttf_extra_static_libraries}")
+string(REGEX REPLACE "^-l" "" _sdl2ttf_extra_static_libraries "${_sdl2ttf_extra_static_libraries}")
+string(REGEX REPLACE ";-l" ";" _sdl2ttf_extra_static_libraries "${_sdl2ttf_extra_static_libraries}")
+
+# All targets are created, even when some might not be requested though COMPONENTS.
+# This is done for compatibility with CMake generated SDL2_ttf-target.cmake files.
+
+if(NOT TARGET SDL2_ttf::SDL2_ttf)
+    add_library(SDL2_ttf::SDL2_ttf STATIC IMPORTED)
+    set_target_properties(SDL2_ttf::SDL2_ttf
+        PROPERTIES
+        IMPORTED_LOCATION "${_sdl2ttf_library}"
+        INTERFACE_INCLUDE_DIRECTORIES "${_sdl2ttf_incdir}"
+        INTERFACE_LINK_LIBRARIES "${_sdl2ttf_extra_static_libraries}"
+    )
+endif()
+
+unset(_sdl2ttf_extra_static_libraries)
+unset(_sdl_arch_subdir)
+unset(_sdl2ttf_incdir)
+unset(_sdl2ttf_library)
+unset(_sdl2ttf_dll)
